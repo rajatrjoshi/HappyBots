3adb9e3fcd63a240a1be7a8884f174f595c73e19
rajatrjoshi <rajatj969@gmail.com>
2021-03-28 12:30:20 +0530

    css changes

--- src/component/SliderData.js
+++ src/component/SliderData.js
@@ -1,4 +1,5 @@
 import carouselimage1 from '../carousel1.png';
+import caoruselimage2 from '../carousel2.jpg'
 
 export const SliderData = [
     {
@@ -7,7 +8,7 @@ export const SliderData = [
     },
     {
       image:
-        'https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80'
+        caoruselimage2
     },
     {
       image:
